59ed38ac84f643db4b4b0e0d0ea422077cb7f355
ANUBHAB CHAUDHURI <91276548+anubhabch10@users.noreply.github.com>
2025-02-10 00:44:32 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function searchProjects() {
-    const placeName = document.getElementById('placeInput').value;
-    const searchUrl = `https://www.google.com/search?q=${placeName} real estate projects under construction`;
-    window.open(searchUrl, '_blank');
-}
